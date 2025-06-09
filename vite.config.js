31148193f918ae564ed3cb0a48b8aa4f4f55115e
Lya Mun <lyamun2@illinois.edu>
2025-06-09 22:45:29 +0900

    fix: update paths to be relative

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [react()],
+    base: '/lyamun.github.io/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
         emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html')
+            }
+        }
     },
     server: {
         port: 3000,
